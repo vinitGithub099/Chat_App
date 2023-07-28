49a35bf62286807294d480687ab037e66fbdc77c
Vinit Kumbhare <studyvinit99@gmail.com>
2023-07-28 23:25:17 +0530

    set up login/signup with JWT

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,86 @@
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcrypt");
+var User = require("../models/user");
+
+exports.signUp = (req, res) => {
+  const user = new User({
+    fullName: req.body.fullName,
+    email: req.body.email,
+    role: req.body.role,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
+
+  user
+    .save()
+    .then((user) => {
+      res.status(200).send({
+        message: "User Registered successfully",
+      });
+    })
+    .catch((error) =>
+      res.status(500).send({
+        message: err,
+      })
+    );
+};
+
+exports.signIn = (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: "User Not found.",
+        });
+      }
+
+      /**
+       * * comparing passwords
+       */
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      /**
+       * * checking if password was valid and send response accordingly
+       */
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!",
+        });
+      }
+      /**
+       * * signing token with user id
+       */
+      var token = jwt.sign(
+        {
+          id: user.id,
+        },
+        process.env.API_SECRET,
+        {
+          expiresIn: 86400,
+        }
+      );
+
+      /**
+       * * responding to client request with user profile success message and  access token .
+       */
+      res.status(200).send({
+        user: {
+          id: user._id,
+          email: user.email,
+          fullName: user.fullName,
+        },
+        message: "Login successfull",
+        accessToken: token,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: error,
+      });
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/user");
+require("dotenv").config();
+const connectDB = require("./db");
+connectDB();
 
 const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(userRoutes);
 
 // start the Express server
 app.listen(PORT, () => {
