3ea1b5dd95bfc6d5bc5c06aab994f59a569d4a90
Vinit Kumbhare <studyvinit99@gmail.com>
2023-12-04 13:28:02 +0530

    added status code 401 for expiration check
    - status 401 -> token not sent
    - ask to login again

--- frontend/src/store/Utils/ActionUtils.js
+++ frontend/src/store/Utils/ActionUtils.js
@@ -1,5 +1,9 @@
 export function handelTokenExpiration(error, dispatch) {
-  if (error && error.response && error.response.status === 403) {
+  if (
+    error &&
+    error.response &&
+    (error.response.status === 403 || error.response.status === 401)
+  ) {
     dispatch({ type: "auth/setTokenExpiration", payload: true });
   } else {
     dispatch({ type: "auth/setTokenExpiration", payload: false });
