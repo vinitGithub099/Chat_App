8a139503429267c168ed2afa90686d94b45a6a18
Vinit Kumbhare <studyvinit99@gmail.com>
2024-06-02 19:40:02 +0530

    updated with comments and modified error hanlding

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,26 +4,34 @@ const ForbiddenError = require("../errors/ForbiddenError");
 const UnauthorizedError = require("../errors/UnauthorizedError");
 
 /**
- * * middleware to check if user has JWT token
+ * * middleware to check if user has access token
  * * it authenticates the user
  * @param {*} next: to call next middleware/route
  */
 const protect = async (req, res, next) => {
-  let token;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      token = req.headers.authorization.split(" ")[1];
-      /* decodes token id */
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const token = req.headers.authorization.split(" ")[1];
+
+      // verify the token and decodes token id
+      const decoded = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
+
+      // find user by the id and attach to req object
       req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        return next(new UnauthorizedError("Not authorized, user not found!"));
+      }
       next();
     } catch (error) {
-      /* Expired token */
+      // Expired token
       return next(new UnauthorizedError("Not authorized, token expired!"));
     }
+  } else {
+    return next(new UnauthorizedError("Not authorized, token missing in request!"));
   }
 };
 
