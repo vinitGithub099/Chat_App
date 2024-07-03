6ac82dfede80d98ebd59a98fa31389beac9088c2
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-03 13:20:57 +0530

    removed conditional rendering from App.jsx as it is not required  now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
-import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import "./index.css";
-const message = "Verifying Credentials";
 
 export default function App() {
-  const loading = useSelector((state) => state.auth.loading);
-
   return (
     <main className="app">
-      {loading.autLogin ? <div>{message}</div> : <Outlet></Outlet>}
+      <Outlet></Outlet>
     </main>
   );
 }
