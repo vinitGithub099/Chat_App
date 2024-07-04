66b68d9cebae63c97b97c708277a3c28ca85965d
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-04 17:43:29 +0530

    modified main.jsx and deleted unnecessary files and folders

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,12 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import persistStore from "redux-persist/es/persistStore";
 import { PersistGate } from "redux-persist/integration/react";
-import { configureAxios } from "./configs/axiosConfigs";
 import "./index.css";
 import { router } from "./routes/AppRoutes";
 import store from "./store/store";
-
-import { ThemeProvider } from "@material-tailwind/react";
-
-// export const chatSocket = socket;
-
-configureAxios();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
