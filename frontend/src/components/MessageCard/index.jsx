9186529241606fdf10e0ee5b38bfa05f85695a81
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-05 12:39:25 +0530

    ui change in sender and receiver message cards

--- frontend/src/components/MessageCard/index.jsx
+++ frontend/src/components/MessageCard/index.jsx
@@ -1,5 +1,6 @@
 import { Avatar } from "@material-tailwind/react";
 import cx from "classnames";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import logo from "../../assets/profile-user_64572.png";
 import { formatTimestamp } from "../../helpers/helpers";
@@ -7,23 +8,32 @@ import classes from "./index.module.css";
 
 const MessageCard = ({ sender, content, updatedAt }) => {
   const user = useSelector((state) => state.auth.user);
+  const [isSender] = useState(sender._id === user._id);
+
+  const buildSenderName = () => (!isSender ? sender.name : "");
 
   return (
     <div
       className={cx(
         classes.messageCard,
         {
-          [classes.floatLeft]: sender._id !== user._id,
+          [classes.floatLeft]: !isSender,
         },
-        { [classes.floatRight]: sender._id === user._id }
+        { [classes.floatRight]: isSender }
       )}
     >
       <div className={classes.triangle}></div>
       {sender._id !== user._id ? (
         <Avatar src={logo} alt="user" size="xs" className={classes.avatar} />
       ) : null}
-      <div className={classes.details}>
-        <div className={classes.senderName}>{sender.name}</div>
+      <div
+        className={cx(
+          classes.details,
+          { [classes.nonSenderBg]: !isSender },
+          { [classes.senderBg]: isSender }
+        )}
+      >
+        <div className={classes.senderName}>{buildSenderName()}</div>
         <div className={classes.messageGroup}>
           <p className={classes.content}>{content}</p>
           <p className={classes.timeStamp}>{formatTimestamp(updatedAt)}</p>
