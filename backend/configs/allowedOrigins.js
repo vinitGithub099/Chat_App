409d599049488449811a71f8ea0f4d8b8ca41adb
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-07 20:20:28 +0530

    updated origin urls using env variables

--- backend/configs/allowedOrigins.js
+++ backend/configs/allowedOrigins.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
 import express from "express";
 const app = express();
+dotenv.config();
 
 export const allowedOrigins =
   app.settings.env === "development"
-    ? [process.env.FRONTEND_DEV_URL]
+    ? [process.env.FRONTEND_DEV_URL1, process.env.FRONTEND_DEV_URL2]
     : [process.env.FRONTEND_PROD_URL];
