a4e6e77d4333b77e46db8965f2aa559f610b6091
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-09 16:19:39 +0530

    created auth notification component

---
+++ frontend/src/components/Notification/AuthNotification/index.jsx
@@ -0,0 +1,66 @@
+import { Button, Typography } from "@material-tailwind/react";
+import cx from "classnames";
+import { toast } from "react-hot-toast";
+import { AiOutlineClose } from "react-icons/ai";
+import { FaCheckCircle } from "react-icons/fa";
+import { MdError } from "react-icons/md";
+import { AUTH_NOTIFICATION_STATUS } from "../../../constants/authNotficationTypes";
+import classes from "./index.module.css";
+
+const AuthNotification = ({ status, action, id }) => {
+  const removeToast = () => toast.remove(id);
+  const getNotificationStyle = () => {
+    switch (status) {
+      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+        return classes.authSuccess;
+      case AUTH_NOTIFICATION_STATUS.FAILURE:
+        return classes.authFailure;
+      default:
+        return "";
+    }
+  };
+
+  const getDefaultMessage = () => {
+    switch (status) {
+      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+        return `${capitalizeFirstLetter(action)} successful!`;
+      case AUTH_NOTIFICATION_STATUS.FAILURE:
+        return `${capitalizeFirstLetter(action)} failed.`;
+      default:
+        return "Operation status unknown.";
+    }
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string?.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const renderAuthIcon = () => {
+    switch (status) {
+      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+        return <FaCheckCircle size={20} />;
+        case AUTH_NOTIFICATION_STATUS.FAILURE:
+        return <MdError size={20} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={cx(classes.authToastContainer, getNotificationStyle())}>
+      {renderAuthIcon()}
+      <Typography variant="small" className={classes.authToastMessage}>
+        {getDefaultMessage()}
+      </Typography>
+      <Button
+        variant="text"
+        onClick={removeToast}
+        className={classes.authToastDimissBtn}
+      >
+        <AiOutlineClose />
+      </Button>
+    </div>
+  );
+};
+
+export default AuthNotification;
