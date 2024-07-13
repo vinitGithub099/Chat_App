645aa988118b8cae7efaac828a8200f5580cc726
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-13 11:42:11 +0530

    optimized code and remvoed latest messages count feature

--- frontend/src/components/ChatCard/index.jsx
+++ frontend/src/components/ChatCard/index.jsx
@@ -1,45 +1,22 @@
 import { Avatar, Typography } from "@material-tailwind/react";
 import cx from "classnames";
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import userIcon from "../../assets/profile-user_64572.png";
 import { MENU_ITEMS } from "../../constants/sideMenu";
 import { buildChatName, formatTimestamp } from "../../helpers/helpers";
-import { chatSocket } from "../../main";
-import { updateCurrentChat } from "../../store/Features/Chat/ChatSlice";
-import { setActitvityLabel } from "../../store/Features/UI/UISlice";
+import useTypingStatus from "../../hooks/useTypingStatus";
+import { updateCurrentChat } from "../../store/Features/Chat/chatSlice";
+import { setActitvityLabel } from "../../store/Features/UI/uiSlice";
 import classes from "./index.module.css";
 
 const ChatCard = (props) => {
-  const { _id, chatName, latestMessage, unreadMsgCount, updatedAt } = props;
+  const { _id, chatName, latestMessage } = props;
   const currentChat = useSelector((state) => state.chat.currentChat);
   const user = useSelector((state) => state.auth.user);
-  const [typingStatus, updateTypingStatus] = useState({
-    isTyping: false,
-    name: null,
-  });
   const dispatch = useDispatch();
 
-  // Continuously listen typing events for each chat card separately
-  // Can't be gloablised
-  useEffect(() => {
-    const listenStartTyping = () => {
-      chatSocket.on("typing", ({ room, user }) => {
-        if (room?._id === _id)
-          updateTypingStatus({ isTyping: true, name: user?.name });
-      });
-    };
-
-    const listenStopTyping = () => {
-      chatSocket.on("stop typing", ({ room }) => {
-        if (room?._id === _id)
-          updateTypingStatus({ isTyping: false, name: null });
-      });
-    };
-
-    listenStartTyping();
-    listenStopTyping();
-  });
+  // Continuously listen typing events for each chat card
+  const [typingStatus] = useTypingStatus(_id);
 
   const handelSelectChat = () => {
     dispatch(updateCurrentChat(props));
@@ -62,7 +39,9 @@ const ChatCard = (props) => {
       <div className={classes.chatInfo}>
         <div className={classes.chatInfoField}>
           <h4 className={classes.chatName}>{buildChatName(props, user)}</h4>
-          <p className={classes.updates}>{formatTimestamp(updatedAt) ?? ""}</p>
+          <p className={classes.updates}>
+            {formatTimestamp(latestMessage?.updatedAt) ?? ""}
+          </p>
         </div>
         <div className={classes.chatInfoField}>
           {typingStatus.isTyping ? (
@@ -79,8 +58,7 @@ const ChatCard = (props) => {
               )}
             </p>
           )}
-          {/* replace this with correct/new feature */}
-          <p className={classes.unreadMsgCount}>{unreadMsgCount || 10}</p>
+          {/* build a functionality to show unread messages here */}
         </div>
       </div>
     </div>
