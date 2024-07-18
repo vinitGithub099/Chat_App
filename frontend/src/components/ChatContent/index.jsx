79f3a94f6c6d6c8ac32923caedba0cbe5a2714b3
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-18 18:32:04 +0530

    Refactor: rename errorFallback to defaultFallback Feature: implement ChatForm to create new group chat

--- frontend/src/components/ChatContent/index.jsx
+++ frontend/src/components/ChatContent/index.jsx
@@ -1,23 +1,29 @@
 import { Button, Drawer, Typography } from "@material-tailwind/react";
 import cx from "classnames";
+import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+import { IoCreateOutline } from "react-icons/io5";
 import { TbReload } from "react-icons/tb";
 import { useDispatch, useSelector } from "react-redux";
 import { MENU_ITEMS } from "../../constants/sideMenu";
 import { toggleSidebar } from "../../store/Features/UI/uiSlice";
+import ChatForm from "../ChatForm";
 import ChatList from "../ChatList";
 import ChatSidebar from "../ChatSidebar";
 import classes from "./index.module.css";
 
 const ChatContent = ({ className }) => {
   const { isSidebarOpen, contentLabel } = useSelector((state) => state.ui);
+  const [isChatFormOpen, setisChatFormOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleSidebar = (e) => {
     dispatch(toggleSidebar(e?.currentTarget?.value === "true" || e));
   };
 
-  const errorFallback = (
+  const toggleChatForm = () => setisChatFormOpen((prev) => !prev);
+
+  const defaultFallback = (
     <Button variant="outlined" className={classes.errBtn} ripple={false}>
       Refresh
       <TbReload size={20} />
@@ -35,7 +41,7 @@ const ChatContent = ({ className }) => {
           </Typography>
         );
       default:
-        return errorFallback;
+        return defaultFallback;
     }
   };
 
@@ -47,16 +53,23 @@ const ChatContent = ({ className }) => {
           className={classes.menuIcon}
           onClick={handleSidebar}
           value={true}
-          ripple={false}
         >
           <AiOutlineMenu size={20} />
         </Button>
         <Typography variant="h5">{contentLabel}</Typography>
+        <Button
+          variant="text"
+          className={classes.channelBtn}
+          onClick={toggleChatForm}
+        >
+          <IoCreateOutline size={25} />
+        </Button>
       </div>
       <div className={classes.content}>{renderContent(contentLabel)}</div>
       <Drawer placement="left" open={!!isSidebarOpen} onClose={handleSidebar}>
         <ChatSidebar />
       </Drawer>
+      <ChatForm {...{ isChatFormOpen, toggleChatForm }} />
     </div>
   );
 };
