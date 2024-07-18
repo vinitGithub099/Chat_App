b01c903546c5ea71bbacc75e8c5e0837d15be044
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-18 18:09:19 +0530

    Feature: custom option component for UserSelect component

---
+++ frontend/src/components/UserSelect/Option/index.jsx
@@ -0,0 +1,21 @@
+import cx from "classnames";
+import UserCard from "../../UserCard";
+import classes from "./index.module.css";
+
+const Option = (props) => {
+  const label = props.data.label;
+  return (
+    <div
+      {...props.innerProps}
+      className={cx(
+        { [classes.focus]: props.isFocused },
+        { [classes.select]: props.isSelected },
+        classes.option
+      )}
+    >
+      <UserCard user={label} avatarSize={"sm"} />
+    </div>
+  );
+};
+
+export default Option;
