3faacc81eaf8b80adce2ce23c4f51053efc1cec8
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-18 18:18:20 +0530

    Refactor: changes made by prettier code formatter

--- frontend/src/components/UserCard/index.jsx
+++ frontend/src/components/UserCard/index.jsx
@@ -6,7 +6,11 @@ import classes from "./index.module.css";
 const UserCard = ({ className, avatarSize, user, onClick }) => {
   return (
     <div className={cx(classes.userCardContainer, className)} onClick={onClick}>
-      <Avatar src={user.pic || userLogo} size={avatarSize} className={classes.avatar} />
+      <Avatar
+        src={user.pic || userLogo}
+        size={avatarSize}
+        className={classes.avatar}
+      />
       <div className={classes.userInfo}>
         <Typography variant="small" className={classes.userName}>
           {user.name ?? ""}
