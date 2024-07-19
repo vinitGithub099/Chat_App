f27feb80e71de17a83799ef03501bccf4fdcefd9
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-19 12:48:56 +0530

    Move: extract messages container from message view component to improve readability

---
+++ frontend/src/components/MessageView/MessagesContainer/index.jsx
@@ -0,0 +1,51 @@
+import { Spinner, Typography } from "@material-tailwind/react";
+import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { useLazyFetchChatMessagesQuery } from "../../../store/Services/messageAPI";
+import MessageCard from "../../MessageCard";
+import classes from "./index.module.css";
+
+const MessagesContainer = () => {
+  const currentChat = useSelector((state) => state.chat.currentChat);
+  const messages = useSelector((state) => state.message.messages);
+  const [fetchCurrChatMessages, { isLoading }] =
+  useLazyFetchChatMessagesQuery();
+  const messagesEndRef = useRef(null);
+
+  // fetch chat messages of the current chat when this compoenent mounts and  updates
+  useEffect(() => {
+    if (currentChat) {
+      fetchCurrChatMessages(currentChat._id).catch(console.error);
+    }
+  }, [currentChat, fetchCurrChatMessages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <div className={classes.messagesContainer}>
+      {isLoading ? (
+        <div className={classes.spinLoaderContainer}>
+          <Spinner className={classes.spinner} />
+          <span>Loading messages</span>
+        </div>
+      ) : messages?.length ? (
+        messages.map((message) => (
+          <MessageCard key={message._id} {...message} />
+        ))
+      ) : (
+        <Typography variant="h6" className={classes.noMessage}>
+          No messages to show!
+        </Typography>
+      )}
+      <div ref={messagesEndRef} />
+    </div>
+  );
+};
+
+export default MessagesContainer;
