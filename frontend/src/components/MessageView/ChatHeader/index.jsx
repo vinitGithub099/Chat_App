cd54e920cb28b568d38c32238f9f6ee1f4c20165
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-20 13:33:46 +0530

    Refactor: optimize the calls to buildChatName function

--- frontend/src/components/MessageView/ChatHeader/index.jsx
+++ frontend/src/components/MessageView/ChatHeader/index.jsx
@@ -1,17 +1,24 @@
-import { Avatar, Button, Typography } from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
+import { useMemo } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
-import userIcon from "../../../assets/profile-user_64572.png";
 import { buildChatName } from "../../../helpers/helpers";
 import useTypingStatus from "../../../hooks/useTypingStatus";
 import { updateCurrentChat } from "../../../store/Features/Chat/chatSlice";
 import { setActitvityLabel } from "../../../store/Features/UI/uiSlice";
+import ChatAvatar from "../../ChatAvatar";
+import UserAvatar from "../../UserAvatar";
 import classes from "./index.module.css";
 
 const ChatHeader = ({ handleChatInfo }) => {
   const currentChat = useSelector((state) => state.chat.currentChat);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
+
+  const chatName = useMemo(
+    () => buildChatName(currentChat, user),
+    [currentChat, user]
+  );
 
   // Continuously listen to the typing events of currently selected chat
   const [typingStatus] = useTypingStatus(currentChat?._id);
@@ -31,14 +38,18 @@ const ChatHeader = ({ handleChatInfo }) => {
       >
         <IoIosArrowBack size={20} />
       </Button>
-      <Avatar src={userIcon} alt={""} size="sm" className={classes.avatar} />
+      {currentChat.isGroupChat ? (
+        <ChatAvatar id={currentChat._id} src={""} />
+      ) : (
+        <UserAvatar id={currentChat._id} userName={chatName} size="md" />
+      )}
       <div className={classes.chatHeaderDetails}>
         <Typography
           variant="h6"
           className={classes.chatName}
           onClick={handleChatInfo}
         >
-          {buildChatName(currentChat, user)}
+          {chatName}
         </Typography>
         {typingStatus.isTyping ? (
           <Typography
