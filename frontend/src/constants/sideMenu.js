000c771e0e95f010d7a3296e6e0b83f3438f51bb
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-21 19:26:58 +0530

    Refactor: add Home in menu items

--- frontend/src/constants/sideMenu.js
+++ frontend/src/constants/sideMenu.js
@@ -1,10 +1,14 @@
-import { FaUser } from "react-icons/fa";
+import { FaHome, FaUser } from "react-icons/fa";
 import { FaCircleNotch } from "react-icons/fa6";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MdOutlineChat } from "react-icons/md";
 import { RiLogoutCircleLine } from "react-icons/ri";
 
 export const MENU_ITEMS = {
+  HOME: {
+    label: "Home",
+    icon: FaHome,
+  },
   CHATS: {
     label: "Chats",
     icon: MdOutlineChat,
