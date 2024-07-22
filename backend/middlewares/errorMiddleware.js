02cdcda9bb59351a64c256d0af3a916c1255ebc4
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-22 19:19:43 +0530

    Fix: change error class from Error to AppError

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,5 +1,7 @@
+import AppError from "../errors/AppError";
+
 export const notFound = (req, res, next) => {
-  const error = new Error(`Not found - ${req.originUrl}`);
+  const error = new AppError(`Not found - ${req.originUrl}`);
   res.status(404);
   next(error);
 };
