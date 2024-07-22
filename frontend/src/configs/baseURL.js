666cbeaf5c7852662bc1be6add4285dd97638bd7
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-22 19:19:17 +0530

    Fix: update backend url

--- frontend/src/configs/baseURL.js
+++ frontend/src/configs/baseURL.js
@@ -1,5 +1,5 @@
 /* backend API base url */
 export const BASE_URL = import.meta.env.PROD
-  ? "https://chat-app-backend-f9vy.onrender.com"
+  ? "https://chat-app-myym.onrender.com"
   : "http://localhost:5000";
 
