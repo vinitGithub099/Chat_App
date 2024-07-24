4c2de3eceb112bd0d4a8633e5757db83d17abc1f
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-24 23:10:00 +0530

    Refactor: remove formatTimestamp and add two new helper functions for fromatting time using moment.js

--- frontend/src/helpers/helpers.js
+++ frontend/src/helpers/helpers.js
@@ -1,24 +1,11 @@
+import moment from "moment";
+
 // build the name of chat provided chat and user
 export const buildChatName = (chat, user) =>
   chat?.isGroupChat
     ? chat?.chatName
     : chat?.users?.find((member) => member._id !== user?._id)?.name ??
       "Unknown";
-
-// format timestamp in hh:mm format
-export const formatTimestamp = (timestamp) => {
-  if (!timestamp) {
-    return "";
-  }
-
-  const date = new Date(timestamp);
-
-  if (isNaN(date.getTime())) {
-    return "";
-  }
-
-  return date.toTimeString().slice(0, 5);
-};
 
 // generate unique color from _id (ObjectId)
 export const stringToColor = (str) => {
@@ -32,4 +19,42 @@ export const stringToColor = (str) => {
     color += value.toString(16).padStart(2, "0");
   }
   return color;
+};
+
+
+export const getFormattedDateLabel = (date) => {
+  const now = moment();
+  const messageDate = moment(date);
+
+  if (messageDate.isSame(now, 'day')) {
+    return 'Today';
+  } else if (messageDate.isSame(now.subtract(1, 'days'), 'day')) {
+    return 'Yesterday';
+  } else if (messageDate.isAfter(now.startOf('week'))) {
+    return messageDate.format('dddd');
+  } else {
+    return messageDate.format('DD-MM-YYYY');
+  }
+};
+
+
+// group messages by data
+export const groupMessagesByDate = (messages) => {
+  const messageGroups = messages.reduce((acc, message) => {
+    const messageDate = moment(message.updatedAt).format('YYYY-MM-DD');
+    if (!acc[messageDate]) {
+      acc[messageDate] = [];
+    }
+    acc[messageDate].push(message);
+    return acc;
+  }, {});
+
+  const groupedMessages = Object.keys(messageGroups).map((date) => {
+    return {
+      date,
+      messages: messageGroups[date]
+    };
+  });
+
+  return groupedMessages;
 };
