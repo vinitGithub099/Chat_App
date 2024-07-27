083a733383d8f9ce2a0f4eeab2c5123db1bd5cba
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-27 15:34:57 +0530

    Feature: get real-time online users update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import "./index.css";
 import { chatSocket } from "./main";
 import NotificationProvider from "./providers/NotificationProvider";
+import { setOnlineUsers } from "./store/Features/OnlineUsers/onlineUsersSlice";
 
 export default function App() {
   const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!user) return;
@@ -14,11 +16,20 @@ export default function App() {
     chatSocket.emit("setup", user);
 
     chatSocket.on("connected", () => {
-      console.log("init chat socket id: ", chatSocket.id);
+      if (!import.meta.env.PROD) {
+        console.log("init chat socket id: ", chatSocket.id);
+      }
+    });
+
+    chatSocket.on("online users", (joinedUsers) => {
+      if (!import.meta.env.PROD) {
+        console.log("online users ", joinedUsers);
+      }
+      dispatch(setOnlineUsers(joinedUsers));
     });
 
     return () => chatSocket.disconnect();
-  }, [user]);
+  }, [dispatch, user]);
 
   return (
     <main className="app">
