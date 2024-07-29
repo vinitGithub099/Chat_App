c2e5a77704e427ae094a816b3b80191c7e542883
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-29 13:46:17 +0530

    Refactor: fix notfication chat name and text content

--- frontend/src/components/Notification/MessageNotification/index.jsx
+++ frontend/src/components/Notification/MessageNotification/index.jsx
@@ -2,20 +2,35 @@ import { Avatar, Button, Typography } from "@material-tailwind/react";
 import toast from "react-hot-toast";
 import { AiOutlineClose } from "react-icons/ai";
 import userLogo from "../../../assets/profile-user_64572.png";
+import {
+  BUTTON_VARIANT,
+  TYPOGRAPHY_VARIANT,
+} from "../../../constants/variants";
 import classes from "./index.module.css";
-import { BUTTON_VARIANT, TYPOGRAPHY_VARIANT } from "../../../constants/variants";
 
-const MessageNotification = ({ sender, content, id }) => {
+const MessageNotification = ({ sender, content, chat, id }) => {
   const removeToast = () => toast.remove(id);
+
+  const builtMessageContent = () => `${sender?.name}: ${content}`;
+
+  const buildChatName = () =>
+    chat?.isGroupChat ? chat.chatName : sender?.name;
+
   return (
     <div className={classes.msgToastContainer}>
       <Avatar size="xs" src={userLogo} className={classes.msgToastAvatar} />
       <div className={classes.msgInfo}>
-        <Typography variant={TYPOGRAPHY_VARIANT.H6} className={classes.msgSender}>
-          {sender?.name ?? "Unknown"}
+        <Typography
+          variant={TYPOGRAPHY_VARIANT.H6}
+          className={classes.msgSender}
+        >
+          {buildChatName()}
         </Typography>
-        <Typography variant={TYPOGRAPHY_VARIANT.SMALL} className={classes.msgContent}>
-          {content}
+        <Typography
+          variant={TYPOGRAPHY_VARIANT.SMALL}
+          className={classes.msgContent}
+        >
+          {builtMessageContent()}
         </Typography>
       </div>
       <Button
