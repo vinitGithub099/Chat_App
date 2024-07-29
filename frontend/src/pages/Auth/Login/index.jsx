46e594aeec976dcc022a2f5c79706d9a56af7848
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-29 16:51:09 +0530

    Feature: update UI if user is logged in and add notifications for login success and failure

--- frontend/src/pages/Auth/Login/index.jsx
+++ frontend/src/pages/Auth/Login/index.jsx
@@ -1,16 +1,23 @@
 import { Button, Input, Typography } from "@material-tailwind/react";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaHouseUser } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import AppAvatar, { AVATAR_TYPE } from "../../../components/AppAvatar";
+import {
+  AUTH_NOTIFICATION_ACTION,
+  AUTH_NOTIFICATION_STATUS,
+} from "../../../constants/authNotficationTypes";
 import { FORM_FIELD } from "../../../constants/formFields";
 import { EMAIL_REGEX } from "../../../constants/regex";
+import { TOAST_TYPE } from "../../../constants/toastTypes";
 import {
   BUTTON_VARIANT,
   INPUT_VARIANT,
   TYPOGRAPHY_VARIANT,
 } from "../../../constants/variants";
+import useNotification from "../../../hooks/useNotification";
 import { useLoginMutation } from "../../../store/Services/authAPI";
 import classes from "../index.module.css";
 import { FIELD_NAME } from "./fieldNames";
@@ -22,18 +29,31 @@ const LoginPage = () => {
     handleSubmit,
   } = useForm({ mode: "onChange" });
   const navigate = useNavigate();
-  const user = useSelector(state => state.auth.user);
-  const [successMessage, setSuccessMessage] = useState("");
+  const user = useSelector((state) => state.auth.user);
+  const { notify } = useNotification();
 
   const [login] = useLoginMutation();
 
   const loginUser = async (userData) => {
     try {
       await login(userData);
-      // navigate to home page afer successful login
-      navigate("/");
+      notify(
+        {
+          toastType: TOAST_TYPE.AUTH,
+          status: AUTH_NOTIFICATION_STATUS.SUCCESS,
+          action: AUTH_NOTIFICATION_ACTION.LOGIN,
+        },
+        { position: "top-right" }
+      );
     } catch (error) {
-      console.log(error);
+      notify(
+        {
+          toastType: TOAST_TYPE.AUTH,
+          status: AUTH_NOTIFICATION_STATUS.FAILURE,
+          action: AUTH_NOTIFICATION_ACTION.LOGIN,
+        },
+        { position: "top-right" }
+      );
     }
   };
 
@@ -41,94 +61,115 @@ const LoginPage = () => {
     loginUser(formData);
   };
 
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <section className={classes.container}>
-     {!successMessage ?  <div className={classes.subContainer}>
-        <div className={classes.icon}>
-          <FaHouseUser size={80} />
-        </div>
-        <div className={classes.formContainer}>
-          <Typography variant={TYPOGRAPHY_VARIANT.H4}>User Login</Typography>
-          <form
-            className={classes.form}
-            onSubmit={handleSubmit(handleFormSubmit)}
-            noValidate
-          >
-            <div className={classes.formField}>
-              <Input
-                variant={INPUT_VARIANT.OUTLINED}
-                className={classes.input}
-                type={FORM_FIELD.EMAIL}
-                placeholder="user@gmail.com"
-                labelProps={{ className: classes.labelProps }}
-                {...register(FIELD_NAME.EMAIL, {
-                  required: {
-                    value: true,
-                    message: "Please enter your email!",
-                  },
-                  pattern: {
-                    value: EMAIL_REGEX,
-                    message: "Invalid email!",
-                  },
-                })}
-              />
-              {errors?.[FIELD_NAME.EMAIL] && (
-                <Typography
-                  variant={TYPOGRAPHY_VARIANT.SMALL}
-                  className={classes.error}
-                >
-                  {errors[FIELD_NAME.EMAIL].message}
-                </Typography>
-              )}
-            </div>
-            <div className={classes.formField}>
-              <Input
-                variant={INPUT_VARIANT.OUTLINED}
-                className={classes.input}
-                type={FORM_FIELD.PASSWORD}
-                placeholder="********"
-                labelProps={{ className: classes.labelProps }}
-                {...register(FIELD_NAME.PASSWORD, {
-                  required: {
-                    value: true,
-                    message: "Please enter your password!",
-                  },
-                })}
-              />
-              {errors?.[FIELD_NAME.PASSWORD] && (
-                <Typography
-                  variant={TYPOGRAPHY_VARIANT.SMALL}
-                  className={classes.error}
-                >
-                  {errors[FIELD_NAME.PASSWORD].message}
-                </Typography>
-              )}
-            </div>
-            <Button
-              variant={BUTTON_VARIANT.TEXT}
-              type="submit"
-              className={classes.submitBtn}
-              fullWidth
-              disabled={!isValid}
+      {!user ? (
+        <div className={classes.subContainer}>
+          <div className={classes.icon}>
+            <FaHouseUser size={80} />
+          </div>
+          <div className={classes.formContainer}>
+            <Typography variant={TYPOGRAPHY_VARIANT.H4}>User Login</Typography>
+            <form
+              className={classes.form}
+              onSubmit={handleSubmit(handleFormSubmit)}
+              noValidate
             >
-              Login
-            </Button>
-            <div className={classes.linkContainer}>
-              <Typography variant={TYPOGRAPHY_VARIANT.SMALL}>
-                {`Don't have an account?`}
-                <span className={classes.link}>
-                  <Link to="/register">Register</Link>
-                </span>
-              </Typography>
-            </div>
-          </form>
+              <div className={classes.formField}>
+                <Input
+                  variant={INPUT_VARIANT.OUTLINED}
+                  className={classes.input}
+                  type={FORM_FIELD.EMAIL}
+                  placeholder="user@gmail.com"
+                  labelProps={{ className: classes.labelProps }}
+                  {...register(FIELD_NAME.EMAIL, {
+                    required: {
+                      value: true,
+                      message: "Please enter your email!",
+                    },
+                    pattern: {
+                      value: EMAIL_REGEX,
+                      message: "Invalid email!",
+                    },
+                  })}
+                />
+                {errors?.[FIELD_NAME.EMAIL] && (
+                  <Typography
+                    variant={TYPOGRAPHY_VARIANT.SMALL}
+                    className={classes.error}
+                  >
+                    {errors[FIELD_NAME.EMAIL].message}
+                  </Typography>
+                )}
+              </div>
+              <div className={classes.formField}>
+                <Input
+                  variant={INPUT_VARIANT.OUTLINED}
+                  className={classes.input}
+                  type={FORM_FIELD.PASSWORD}
+                  placeholder="********"
+                  labelProps={{ className: classes.labelProps }}
+                  {...register(FIELD_NAME.PASSWORD, {
+                    required: {
+                      value: true,
+                      message: "Please enter your password!",
+                    },
+                  })}
+                />
+                {errors?.[FIELD_NAME.PASSWORD] && (
+                  <Typography
+                    variant={TYPOGRAPHY_VARIANT.SMALL}
+                    className={classes.error}
+                  >
+                    {errors[FIELD_NAME.PASSWORD].message}
+                  </Typography>
+                )}
+              </div>
+              <Button
+                variant={BUTTON_VARIANT.TEXT}
+                type="submit"
+                className={classes.submitBtn}
+                fullWidth
+                disabled={!isValid}
+              >
+                Login
+              </Button>
+              <div className={classes.linkContainer}>
+                <Typography variant={TYPOGRAPHY_VARIANT.SMALL}>
+                  {`Don't have an account?`}
+                  <span className={classes.link}>
+                    <Link to="/register">Register</Link>
+                  </span>
+                </Typography>
+              </div>
+            </form>
+          </div>
         </div>
-      </div> :
-      <div className="w-full border">
-        <Typography variant={TYPOGRAPHY_VARIANT.H2}>Hey {user?.name}!</Typography>
-        <Typography variant={TYPOGRAPHY_VARIANT.LEAD}>You have logged in successfully!</Typography>
-        <Typography variant={TYPOGRAPHY_VARIANT.LEAD}>Redirecting to Home!</Typography>
-      </div>}
+      ) : (
+        <div className={classes.loggedIn}>
+          <Button
+            variant={BUTTON_VARIANT.TEXT}
+            className={classes.backBtn}
+            onClick={handleClick}
+          >
+            <IoIosArrowBack size={20} />
+            Back
+          </Button>
+          <Typography variant={TYPOGRAPHY_VARIANT.H2}>
+            Hey {user?.name}!
+          </Typography>
+          <div className={classes.avatarContainer}>
+            <AppAvatar entity={user} size="2xl" type={AVATAR_TYPE.USER} />
+          </div>
+          <Typography variant={TYPOGRAPHY_VARIANT.LEAD}>
+            You have currently logged in!
+          </Typography>
+        </div>
+      )}
     </section>
   );
 };
