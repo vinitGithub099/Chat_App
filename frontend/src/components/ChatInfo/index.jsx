3a2d6b9c89ef7a69e1b0c7af8b8174041512f690
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-30 00:24:17 +0530

    Update: handle 0 member list case, add spinner overlay, conditionally render dialog box and refactor code disable any click on chatInfo until removeGroupMember response is received conditonally render dialog components to refresh them on every render refactor code as per changes

--- frontend/src/components/ChatInfo/index.jsx
+++ frontend/src/components/ChatInfo/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Chip, Typography } from "@material-tailwind/react";
+import { Button, Chip, Spinner, Typography } from "@material-tailwind/react";
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineInfoCircle } from "react-icons/ai";
 import { IoExitOutline } from "react-icons/io5";
@@ -15,12 +15,14 @@ import UserCard from "../UserCard";
 import classes from "./index.module.css";
 import LeaveChatDialog from "./LeaveChatDialog";
 
+/* Task: Add UI/UX to show the result removing user */
+
 const ChatInfo = ({ openChatInfo, handleChatInfo }) => {
   const currentChat = useSelector((state) => state.chat.currentChat);
   const user = useSelector((state) => state.auth.user);
   const [openLeaveChat, setOpenLeaveChat] = useState(false);
   const [openAddMember, setOpenAddMember] = useState(false);
-  const [removeGroupMember] = useRemoveGroupMemberMutation();
+  const [removeGroupMember, { isLoading }] = useRemoveGroupMemberMutation();
   const dispatch = useDispatch();
 
   const handleLeaveChat = () => setOpenLeaveChat((prev) => !prev);
@@ -28,13 +30,10 @@ const ChatInfo = ({ openChatInfo, handleChatInfo }) => {
 
   const handleRemoveGroupMember = async (e) => {
     const memberId = e.currentTarget.value;
-    try {
-      const queryData = { chatId: currentChat?._id, userId: memberId };
-      await removeGroupMember(queryData);
-      dispatch(removeChatMember(queryData));
-    } catch (error) {
-      console.log(error);
-    }
+    const queryData = { chatId: currentChat?._id, userId: memberId };
+    removeGroupMember(queryData).then(() =>
+      dispatch(removeChatMember(queryData))
+    );
   };
 
   const Header = () => (
@@ -53,33 +52,40 @@ const ChatInfo = ({ openChatInfo, handleChatInfo }) => {
   );
 
   const MemberList = () =>
-    currentChat?.users?.length
-      ? currentChat.users.map((member) => (
-          <div key={member._id} className={classes.memberCard}>
-            <UserCard
-              user={{
-                ...member,
-                name: member.name === user.name ? "You" : member.name,
-              }}
-              avatarSize="xs"
-            />
-            {currentChat?.groupAdmin?._id === member?._id ? (
-              <Chip value="Admin" className={classes.adminChip} />
-            ) : currentChat?.groupAdmin?._id === user?._id ? (
-              <Button
-                variant={BUTTON_VARIANT.TEXT}
-                className={classes.removeBtn}
-                ripple={false}
-                disabled={currentChat?.groupAdmin?._id !== user?._id}
-                onClick={handleRemoveGroupMember}
-                value={member?._id}
-              >
-                Remove
-              </Button>
-            ) : null}
-          </div>
-        ))
-      : "Failed to load members!";
+    currentChat?.users?.length ? (
+      currentChat.users.map((member) => (
+        <div key={member._id} className={classes.memberCard}>
+          <UserCard
+            user={{
+              ...member,
+              name: member.name === user?.name ? "You" : member.name,
+            }}
+            avatarSize="sm"
+          />
+          {currentChat?.groupAdmin?._id === member?._id ? (
+            <Chip value="Admin" className={classes.adminChip} />
+          ) : currentChat?.groupAdmin?._id === user?._id ? (
+            <Button
+              variant={BUTTON_VARIANT.TEXT}
+              className={classes.removeBtn}
+              ripple={false}
+              disabled={currentChat?.groupAdmin?._id !== user?._id}
+              onClick={handleRemoveGroupMember}
+              value={member?._id}
+            >
+              Remove
+            </Button>
+          ) : null}
+        </div>
+      ))
+    ) : (
+      <Typography
+        variant={TYPOGRAPHY_VARIANT.SMALL}
+        className={classes.memberListError}
+      >
+        Failed to load members!
+      </Typography>
+    );
 
   const Body = () => (
     <>
@@ -137,9 +143,7 @@ const ChatInfo = ({ openChatInfo, handleChatInfo }) => {
         disabled={!currentChat?.isGroupChat}
       >
         <IoExitOutline size={20} />
-        <Typography variant={TYPOGRAPHY_VARIANT.SMALL}>
-          Exit
-        </Typography>
+        <Typography variant={TYPOGRAPHY_VARIANT.SMALL}>Exit</Typography>
       </Button>
     </>
   );
@@ -158,14 +162,26 @@ const ChatInfo = ({ openChatInfo, handleChatInfo }) => {
       body={<Body />}
       footer={<Footer />}
     >
-      <LeaveChatDialog
-        openLeaveChat={openLeaveChat}
-        handleLeaveChat={handleLeaveChat}
-      />
-      <AddMember
-        openAddMember={openAddMember}
-        handleAddMember={handleAddMember}
-      ></AddMember>
+      {isLoading ? (
+        <div className={classes.overlay}>
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          {openLeaveChat ? (
+            <LeaveChatDialog
+              openLeaveChat={openLeaveChat}
+              handleLeaveChat={handleLeaveChat}
+            />
+          ) : null}
+          {openAddMember ? (
+            <AddMember
+              openAddMember={openAddMember}
+              handleAddMember={handleAddMember}
+            ></AddMember>
+          ) : null}
+        </>
+      )}
     </Modal>
   );
 };
