6a87c78dac6ef14a0a2266e8a6aa87a55c7b762f
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-30 00:16:58 +0530

    Refactor: update the code as per changes and add warning class to getNotificationStyle switch case

--- frontend/src/components/Notification/AuthNotification/index.jsx
+++ frontend/src/components/Notification/AuthNotification/index.jsx
@@ -4,18 +4,20 @@ import { toast } from "react-hot-toast";
 import { AiOutlineClose } from "react-icons/ai";
 import { FaCheckCircle } from "react-icons/fa";
 import { MdError } from "react-icons/md";
-import { AUTH_NOTIFICATION_STATUS } from "../../../constants/authNotficationTypes";
-import classes from "./index.module.css";
+import { NOTIFICATION_STATUS } from "../../../constants/notificationStatus";
 import { BUTTON_VARIANT, TYPOGRAPHY_VARIANT } from "../../../constants/variants";
+import classes from "./index.module.css";
 
 const AuthNotification = ({ status, action, id }) => {
   const removeToast = () => toast.remove(id);
   const getNotificationStyle = () => {
     switch (status) {
-      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+      case NOTIFICATION_STATUS.SUCCESS:
         return classes.authSuccess;
-      case AUTH_NOTIFICATION_STATUS.FAILURE:
+      case NOTIFICATION_STATUS.FAILURE:
         return classes.authFailure;
+      case NOTIFICATION_STATUS.WARNING:
+          return classes.authWarn;
       default:
         return "";
     }
@@ -23,9 +25,9 @@ const AuthNotification = ({ status, action, id }) => {
 
   const getDefaultMessage = () => {
     switch (status) {
-      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+      case NOTIFICATION_STATUS.SUCCESS:
         return `${capitalizeFirstLetter(action)} successful!`;
-      case AUTH_NOTIFICATION_STATUS.FAILURE:
+      case NOTIFICATION_STATUS.FAILURE:
         return `${capitalizeFirstLetter(action)} failed.`;
       default:
         return "Operation status unknown.";
@@ -38,9 +40,9 @@ const AuthNotification = ({ status, action, id }) => {
 
   const renderAuthIcon = () => {
     switch (status) {
-      case AUTH_NOTIFICATION_STATUS.SUCCESS:
+      case NOTIFICATION_STATUS.SUCCESS:
         return <FaCheckCircle size={20} />;
-        case AUTH_NOTIFICATION_STATUS.FAILURE:
+        case NOTIFICATION_STATUS.FAILURE:
         return <MdError size={20} />;
       default:
         return null;
