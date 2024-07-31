d54266c5c86834def09835418adb82ccfa98e040
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-31 13:46:20 +0530

    Update: create constants for items as key value pairs

--- frontend/src/components/Header/profileMenuItems.js
+++ frontend/src/components/Header/profileMenuItems.js
@@ -1,12 +1,12 @@
 import { FaPowerOff, FaUserCircle } from "react-icons/fa";
 
-export const profileMenuItems = [
-  {
+export const profileMenuItems = {
+  PROFILE: {
     label: "My Profile",
     icon: FaUserCircle,
   },
-  {
+  SIGN_OUT: {
     label: "Sign Out",
     icon: FaPowerOff,
   },
-];
+};
