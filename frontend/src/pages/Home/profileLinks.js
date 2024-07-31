6943f74154fb77046b6da64d59ccef8103bb57fe
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-31 19:31:48 +0530

    Update: constants for profile links

---
+++ frontend/src/pages/Home/profileLinks.js
@@ -0,0 +1,15 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export const PROFILE_LINKS = {
+  LINKEDIN: {
+    link: "https://www.linkedin.com/in/vinit-kumbhare-5528a221a/",
+    icon: FaLinkedin,
+    label: "LinkedIn"
+  },
+  GITHUB: {
+    link: "https://github.com/vinitGithub099",
+    icon: FaGithub,
+    label: "Github"
+  },
+};
+
