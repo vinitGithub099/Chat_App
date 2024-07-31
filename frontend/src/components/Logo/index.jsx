f4e38ca90d84a1a54bcef13385d335b28e42a5b5
Vinit Kumbhare <studyvinit99@gmail.com>
2024-07-31 19:28:22 +0530

    Update: replace logo avatar with icon form react-icons

--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -1,6 +1,8 @@
-import { Avatar, Typography } from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
 import cx from "classnames";
-import logo from "../../assets/chat.png";
+// import logo from "../../assets/chat.png";
+import { RiChatSmile2Fill } from "react-icons/ri";
+import { APP_NAME } from "../../../appName";
 import classes from "./index.module.css";
 
 const Logo = ({ size = "md", className }) => {
@@ -10,16 +12,40 @@ const Logo = ({ size = "md", className }) => {
     [classes.logoNameMedium]: size === "md",
     [classes.logoNameLarge]: size === "lg",
     [classes.logoNameExtraLarge]: size === "xl",
-    [classes.logoNameDExtraLarge]: size === "xxl",
+    [classes.logoNameDExtraLarge]: size === "2xl",
   });
 
   const containerClass = cx(classes.logoContainer, className);
 
   return (
     <div className={containerClass}>
-      <Avatar src={logo} size={size} variant="square" />
-      <Typography className={logoNameClass}>QuickTalk</Typography>
+      <RiChatSmile2Fill className={cx(classes.avatar, getSizeClass(size))} />
+      <Typography className={logoNameClass}>{APP_NAME}</Typography>
     </div>
   );
 };
+
 export default Logo;
+
+const getSizeClass = (size) => {
+  switch (size) {
+    case "xs":
+      return classes.logoXs;
+    case "sm":
+      return classes.logoSm;
+    case "md":
+      return classes.logoMd;
+    case "lg":
+      return classes.logoLg;
+    case "xl":
+      return classes.logoXl;
+    case "2xl":
+      return classes.logoXxl;
+    case "3xl":
+      return classes.logoXxxl;
+    case "4xl":
+      return classes.logoXxxl;
+    default:
+      return classes.logoMd;
+  }
+};
